f6eb79f8268a09642cd1ab3a4cd88fbc98532587
junglehunter96 <junglehunter96@Gmail.com>
2019-06-12 22:48:11 +0800

    初始化项目结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-
+import store from './store/store'
+import './assets/styles/index.styl'
+import fastclick from 'fastclick'
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
