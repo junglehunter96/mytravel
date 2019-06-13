d2f07f71863d9ffbd980ff7e578a0149a439f06a
junglehunter96 <junglehunter96@Gmail.com>
2019-06-14 02:04:19 +0800

    添加推荐页

---
+++ src/components/home/Recommond.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="recommond">
+    <div class="re-title">猜你喜欢</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .recommond
+    margin-top: 0.25rem
+    .re-title
+      background-color : #f4f4f4
+      padding: .24rem 0 .26rem .25rem
+      font-size: .32rem
+      
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <swiper></swiper>
     <icont></icont>
+    <recommond></recommond>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
   import HomeHeader from '@components/home/Header'
   import Swiper from '@components/home/Swiper'
   import Icont from '@components/home/Icont'
+  import Recommond from '@components/home/Recommond'
   export default {
     data() {
       return {
@@ -19,7 +21,8 @@
     components: {
       HomeHeader,
       Swiper,
-      Icont
+      Icont,
+      Recommond
     }
   }
 </script>
