3a1976f96e8e4ed71682accfdf18bfcc1694f4e2
junglehunter96 <junglehunter96@Gmail.com>
2019-06-15 14:55:22 +0800

    city页面的配置

--- src/views/City.vue
+++ src/views/City.vue
@@ -1,14 +1,59 @@
 <template>
-  <div class="city">
+  <div>
     <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"
+    ></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChange"
+    ></city-alphabet>
   </div>
 </template>
 
 <script>
-import CityHeader from 'components/city/Header.vue'
+import axios from 'axios'
+import CityHeader from 'components/city/Header'
+import CitySearch from 'components/city/Search'
+import CityList from 'components/city/List'
+import CityAlphabet from 'components/city/Alphabet'
 export default {
+  name: 'City',
   components: {
     CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
